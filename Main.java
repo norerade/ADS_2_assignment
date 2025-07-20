f8a28b52474ebd6d7a473e872b687447a3a9ee28
norerade <justrabl2313@gmail.com>
2025-07-20 18:31:19 +0500

    Adding Main class
    Adding Main class, which consists of tests

---
+++ Main.java
@@ -0,0 +1,30 @@
+// Tests
+public class Main {
+    public static void main(String[] args) {
+//        // array
+//        MyArrayList<String> al = new MyArrayList<>();
+//        al.add("test1");
+//        System.out.println(al.get(0));
+//
+//        // linked list
+//        MyLinkedList<String> ll = new MyLinkedList<>();
+//        ll.add("test2");
+//        System.out.println(ll.get(0));
+//
+//        // stack
+//        MyStack<String> st = new MyStack<>();
+//        st.push("test3");
+//        System.out.println(st.pop());
+//
+//        // queue
+//        MyQueue<String> qu = new MyQueue<>();
+//        qu.enqueue("test4");
+//        System.out.println(qu.dequeue());
+//
+//        // min heap
+//        MyMinHeap<Integer> hp = new MyMinHeap<>();
+//        hp.insert(10);
+//        hp.insert(5);
+//        System.out.println(hp.extractMin());
+    }
+}
